0cc01f5937103fc16b0a04c6f3b3b0b4d9bff4d6
Marko Kevac <marko@kevac.org>
2014-11-30 17:04:08 +0300

    almost full rewrite

--- debugcharts.go
+++ debugcharts.go
@@ -1,16 +1,94 @@
 package debugcharts
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"runtime"
+	"sync"
+	"time"
 )
 
+type timestampedDatum struct {
+	Count uint64 `json:"C"`
+	Ts    int64  `json:"T"`
+}
+
+type exportedData struct {
+	BytesAllocated []timestampedDatum
+	GcPauses       []timestampedDatum
+}
+
+const (
+	maxCount int = 86400
+)
+
+var (
+	data      exportedData
+	lastPause uint32
+	mutex     sync.RWMutex
+)
+
+func gatherData() {
+	timer := time.Tick(time.Second)
+
+	for {
+		select {
+		case now := <-timer:
+			nowUnix := now.Unix()
+			var ms runtime.MemStats
+			runtime.ReadMemStats(&ms)
+
+			mutex.Lock()
+			data.BytesAllocated = append(data.BytesAllocated, timestampedDatum{Count: ms.Alloc, Ts: nowUnix})
+			if lastPause == 0 || lastPause != ms.NumGC {
+				data.GcPauses = append(data.GcPauses, timestampedDatum{Count: ms.PauseNs[(ms.NumGC+255)%256], Ts: nowUnix})
+				lastPause = ms.NumGC
+			}
+
+			if len(data.BytesAllocated) > maxCount {
+				data.BytesAllocated = data.BytesAllocated[len(data.BytesAllocated)-maxCount:]
+			}
+
+			if len(data.GcPauses) > maxCount {
+				data.GcPauses = data.GcPauses[len(data.GcPauses)-maxCount:]
+			}
+
+			mutex.Unlock()
+		}
+	}
+}
+
 func init() {
+	/*
+		http.HandleFunc("/debug/charts/data-feed", websocket.Handler(dataFeedHandler))
+	*/
+	http.HandleFunc("/debug/charts/data", dataHandler)
 	http.HandleFunc("/debug/charts/", handleAsset("static/index.html"))
 	http.HandleFunc("/debug/charts/main.js", handleAsset("static/main.js"))
-	/*
-		http.HandleFunc("/debug/charts/proxy", handleProxy)
-	*/
+
+	go gatherData()
+}
+
+/*
+func (s server) dataFeedHandler(ws *websocket.Conn) {
+	defer ws.Close()
+
+	for {
+		select {
+		}
+	}
+}
+*/
+
+func dataHandler(w http.ResponseWriter, r *http.Request) {
+	mutex.RLock()
+	defer mutex.RUnlock()
+
+	w.Header().Set("Content-Type", "application/json")
+
+	encoder := json.NewEncoder(w)
+	encoder.Encode(data)
 }
 
 func handleAsset(path string) func(http.ResponseWriter, *http.Request) {
@@ -30,19 +108,3 @@ func handleAsset(path string) func(http.ResponseWriter, *http.Request) {
 		}
 	}
 }
-
-/*
-func handleProxy(w http.ResponseWriter, r *http.Request) {
-	resp, err := http.Get("http://bumpd.mlan:21104/debug/vars")
-	if err != nil {
-		log.Println(err)
-		// return something
-	}
-
-	headers := w.Header()
-	headers.Set("Content-Type", "application/json")
-	headers.Set("Access-Control-Allow-Origin", "*")
-
-	io.Copy(w, resp.Body)
-}
-*/
--- static/main.js
+++ static/main.js
@@ -1,38 +1,88 @@
 var chart1;
+var chart2;
+
 var lastGC = 0;
+var lastTotalAllocated = 0;
+
+var areaConfDefault = {
+	chart: {
+		type: 'spline',
+		renderTo: 'container2',
+		zoomType: 'x',
+	},
+	loading: {
+		labelStyle: {
+			top: '45%'
+		}
+	},
+	title: {
+		text: 'Memory in use'
+	},
+	subtitle: {
+		text: document.ontouchstart === undefined ?
+			'Click and drag in the plot area to zoom in' :
+			'Pinch the chart to zoom in'
+	},
+	yAxis: {
+		title: {
+			text: 'bytes'
+		}
+	},
+	legend: {
+		enabled: false
+	},
+	plotOptions: {
+		area: {
+			fillColor: {
+				linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1},
+				stops: [
+					[0, Highcharts.getOptions().colors[0]],
+					[1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+				]
+			},
+			marker: {
+				radius: 2
+			},
+			lineWidth: 1,
+			states: {
+				hover: {
+					lineWidth: 1
+				}
+			},
+			threshold: null
+		}
+	},
+
+	series: [{
+		type: 'area',
+		name: 'Memory in use',
+		data: [],
+	}]
+}
+
+function updateGC(data) {
+	var res = [];
+	for (var i=0; i<data.GcPauses.length; i++) {
+		res.push([data.GcPauses[i].T, data.GcPauses[i].C])
+	}
+	chart1.series[0].setData(res, true);
+}
+
+function updateMemAllocated(data) {
+	var res = [];
+	for (var i=0; i<data.BytesAllocated.length; i++) {
+		res.push([data.BytesAllocated[i].T, data.BytesAllocated[i].C])
+	}
+	chart2.series[0].setData(res, true);
+}
 
 function requestData() {
 	$.ajax({
-		url: "/debug/vars",
+		url: "/debug/charts/data",
 		success: function(data) {
-			var pauses = [];
 
-			if (lastGC == 0) {
-				lastGC = data.memstats.NumGC-256;
-				if (lastGC < 0) {
-					lastGC = 1;
-				}
-			}
-
-			for (i=lastGC; i<=data.memstats.NumGC; i++) {
-				pauses[i] = data.memstats.PauseNs[(i+255)%256];
-				console.log("added pause", i)
-			}
-
-			for (i in pauses) {
-				chart1.series[0].addPoint(pauses[i], false, false);
-			}
-
-			lastGC = data.memstats.NumGC+1;
-
-			chart1.redraw();
-
-			// memory in use
-
-			var now = (new Date()).getTime();
-			var memAllocated = data.memstats.Alloc;
-			chart2.series[0].addPoint([now, memAllocated], false, false);
-			chart2.redraw();
+			updateGC(data);
+			updateMemAllocated(data);
 
 			setTimeout(requestData, 10000);
 		},
@@ -40,117 +90,31 @@ function requestData() {
 	});
 }
 
+function loadCallback() {
+	setTimeout(requestData, 100);
+}
+
 $(document).ready(function() {
-	chart1 = new Highcharts.Chart(
-			{
-				chart: {
-					renderTo: 'container1',
-					zoomType: 'x',
-					events: {
-						load: requestData
-					}
-				},
-				title: {
-					text: 'GC pauses'
-				},
-				subtitle: {
-					text: document.ontouchstart === undefined ?
-						'Click and drag in the plot area to zoom in' :
-						'Pinch the chart to zoom in'
-				},
-				yAxis: {
-					title: {
-						text: 'nanoseconds'
-					}
-				},
-				legend: {
-					enabled: false
-				},
-				plotOptions: {
-					area: {
-						fillColor: {
-							linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1},
-							stops: [
-								[0, Highcharts.getOptions().colors[0]],
-								[1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-							]
-						},
-						marker: {
-							radius: 2
-						},
-						lineWidth: 1,
-						states: {
-							hover: {
-								lineWidth: 1
-							}
-						},
-						threshold: null
-					}
-				},
+	var conf = $.extend(true, {}, areaConfDefault);
 
-				series: [{
-					type: 'area',
-					name: 'GC pauses',
-					data: [],
-				}]
-			}
-	);
-	chart2 = new Highcharts.Chart(
-			{
-				chart: {
-					type: 'spline',
-					renderTo: 'container2',
-					zoomType: 'x',
-					events: {
-						load: requestData
-					}
-				},
-				title: {
-					text: 'Memory in use'
-				},
-				subtitle: {
-					text: document.ontouchstart === undefined ?
-						'Click and drag in the plot area to zoom in' :
-						'Pinch the chart to zoom in'
-				},
-				xAxis: {
-					type: 'datetime'
-				},
-				yAxis: {
-					title: {
-						text: 'bytes'
-					}
-				},
-				legend: {
-					enabled: false
-				},
-				plotOptions: {
-					area: {
-						fillColor: {
-							linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1},
-							stops: [
-								[0, Highcharts.getOptions().colors[0]],
-								[1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-							]
-						},
-						marker: {
-							radius: 2
-						},
-						lineWidth: 1,
-						states: {
-							hover: {
-								lineWidth: 1
-							}
-						},
-						threshold: null
-					}
-				},
+	conf.chart.renderTo = 'container1';
+	conf.chart.events = {load: loadCallback};
+	conf.title.text = 'GC pauses';
+	conf.yAxis.title.text = 'nanoseconds';
+	conf.xAxis = {type: 'datetime'};
+	conf.series[0].name = 'GC pauses';
 
-				series: [{
-					type: 'area',
-					name: 'Memory in use',
-					data: [],
-				}]
-			}
-	);
+	chart1 = new Highcharts.Chart(conf);
+
+	var conf = $.extend(true, {}, areaConfDefault);
+
+	conf.chart.renderTo = 'container2';
+	conf.title.text = 'Memory in use';
+	conf.yAxis.title.text = 'bytes';
+	conf.xAxis = {type: 'datetime'};
+	conf.series[0].name = 'Memory in use';
+
+	chart2 = new Highcharts.Chart(conf);
+
+	var conf = $.extend(true, {}, areaConfDefault);
 })
